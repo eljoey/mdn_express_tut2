7c1d0d358c2cb692963bb91e261eea0ec46bcbd6
Joe Hayden <eljoeyy@gmail.com>
2019-10-25 21:00:30 -0500

    book form and validation

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -54,7 +54,7 @@ exports.author_detail = function(req, res, next) {
 }
 
 // Display Author create form on GET.
-exports.author_create_get = function(req, res) {
+exports.author_create_get = function(req, res, next) {
   res.render('author_form', { title: 'Create Author' })
 }
 
